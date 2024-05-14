78c8cc070463b1a5470968dfde3a109ca0fd71a5
weldson01 <weldsoncarlos007@gmail.com>
2024-05-14 19:49:40 -0300

    feat: finalizado desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("3333333", "N1", "222322", 350000);
+Iphone iphone = new Iphone("444444", "Generico 20", "9999999", 1111111);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Atizapi");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("uartizapi");
+
